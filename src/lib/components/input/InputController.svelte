195c54f116a752e9be2ec866323c55f6e32eef27
Yuji Sato <--replace-all>
2024-10-30 22:06:35 +0100

    fix: form table with json

--- src/lib/components/input/InputController.svelte
+++ src/lib/components/input/InputController.svelte
@@ -1,10 +1,9 @@
-<!-- <script lang="ts">
-	import InputGrid from './InputGrid.svelte';
-	import SelectInputMulti from './select-input/base/SelectInputMulti.svelte';
-	import { InputMap } from './select-input';
-	export let formField: any = {};
-	const Input = InputMap[formField.type] || InputGrid;
+<script lang="ts">
+	import type { FormTableField } from '../../../types/form/table-def';
+
+	let { formTableField }: { formTableField: FormTableField } = $props();
 </script>
 
-<Input {label} {name} {type} {className} {formField} /> -->
-<h3>not implemented yet</h3>
+<div>
+	{formTableField.type}
+</div>
