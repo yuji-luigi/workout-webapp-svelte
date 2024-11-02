260daa3192c0e8c079161bca1695997c3048130b
Yuji Sato <--replace-all>
2024-11-02 11:42:18 +0100

    fix(FormGrid.svelte): svelte5 syntax + onSubmit callback

--- src/lib/components/form/FormGrid.svelte
+++ src/lib/components/form/FormGrid.svelte
@@ -1,56 +1,61 @@
 <script lang="ts">
-  import { createFormDataToObject } from "../../helpers/handle-array-form-data";
-  import { sleep } from "../../helpers/sleep";
-  import InputGrid from "../input/InputGrid.svelte";
-  import NewWorkoutModalFormOpenButton from "../open-dialog-button/NewWorkoutModalFormOpenButton.svelte";
+	import { sleep } from '../../helpers/sleep';
 
-  export let loading = false;
-  export let className: string | undefined = "";
-  export let handleSubmit: (event: SubmitEvent) => Promise<void>;
-  async function onsubmit(event: SubmitEvent) {
-    try {
-      loading = true;
-      await handleSubmit(event);
-      await sleep(2000);
-    } catch (error) {
-      throw new Error(error);
-    }
-    loading = false;
-  }
+	let {
+		loading,
+		className,
+		handleSubmit,
+		children
+	}: {
+		loading?: boolean;
+		className?: string | undefined;
+		handleSubmit?: (event: SubmitEvent) => Promise<void>;
+		children?: any;
+	} = $props();
+	async function onsubmit(event: SubmitEvent) {
+		try {
+			loading = true;
+			await handleSubmit?.(event);
+			await sleep(2000);
+		} catch (error: any) {
+			throw new Error(error);
+		}
+		loading = false;
+	}
 </script>
 
 <div class={className}>
-  <fieldset disabled={loading} aria-busy={loading}>
-    <form on:submit={onsubmit}>
-      <slot />
-    </form>
-  </fieldset>
+	<fieldset disabled={loading} aria-busy={loading}>
+		<form {onsubmit}>
+			{@render children?.()}
+		</form>
+	</fieldset>
 </div>
 
 <style>
-  fieldset {
-    container-type: inline-size;
-    border: none;
-    border-radius: 5px;
-    padding: 1rem;
-    transition: opacity 0.3s ease-in-out;
-    &[disabled] {
-      opacity: 0.5;
-    }
-  }
+	fieldset {
+		container-type: inline-size;
+		border: none;
+		border-radius: 5px;
+		padding: 1rem;
+		transition: opacity 0.3s ease-in-out;
+		&[disabled] {
+			opacity: 0.5;
+		}
+	}
 
-  form {
-    max-width: var(--max-width);
-    display: grid;
-    grid-template-columns: repeat(2, auto 1fr);
-    gap: 1rem;
-  }
+	form {
+		max-width: var(--max-width);
+		display: grid;
+		grid-template-columns: repeat(2, auto 1fr);
+		gap: 1rem;
+	}
 
-  @container (max-width: 600px) {
-    form {
-      grid-template-columns: 1fr;
-      max-width: 400px;
-      margin-inline: auto;
-    }
-  }
+	@container (max-width: 600px) {
+		form {
+			grid-template-columns: 1fr;
+			max-width: 400px;
+			margin-inline: auto;
+		}
+	}
 </style>
