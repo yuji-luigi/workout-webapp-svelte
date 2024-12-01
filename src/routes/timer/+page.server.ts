b1f5295e4dda36b62f35d0c24582b1641eefaa92
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-12-01 09:03:11 +0100

    fix: server.ts

--- src/routes/timer/+page.server.ts
+++ src/routes/timer/+page.server.ts
@@ -5,9 +5,10 @@ export const load: PageServerLoad = async ({ fetch }) => {
 	const res = await fetch('https://jsonplaceholder.typicode.com/todos/1').catch((error) => {
 		console.error(error);
 	});
-	const rawTimers = await fetch('/src/data/timers.json');
-	const rawWorkoutTypes = await fetch('/src/data/workout_types.json');
-	const timers = await rawTimers.json();
-	const todo = res ? await res.json() : null;
-	return { timers, workoutTypes: await rawWorkoutTypes.json() };
+	// const rawTimers = await fetch('/src/data/timers.json');
+	// const rawWorkoutTypes = await fetch('/src/data/workout_types.json');
+	// const timers = await rawTimers.json();
+	// const todo = res ? await res.json() : null;
+	// return { timers, workoutTypes: await rawWorkoutTypes.json() };
+	return { timers: [], workoutTypes: [] };
 };
