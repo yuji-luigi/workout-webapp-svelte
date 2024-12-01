2606e59823d3d275c13c1ad8b8b8a7395e63b96d
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-12-01 09:03:28 +0100

    fix: change folder name routine to workout

--- src/lib/components/open-dialog-button/NewExerciseModalFormOpenButton.svelte
+++ src/lib/components/open-dialog-button/NewExerciseModalFormOpenButton.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import type { SvelteComponent } from 'svelte';
-	import { handleOpenDialog } from '../dialog/helper/handleOpenDialog';
-	import WorkoutForm from '../form/workout-form/WorkoutForm.svelte';
-	import { closeDialog } from '../../store/global-dialog-store';
-	import ExerciseForm from '../form/exercise-form/ExerciseForm.svelte';
 	import type { ExerciseJoined } from '../../../types/db/exercise';
+	import { closeDialog } from '../../store/global-dialog-store';
+	import { handleOpenDialog } from '../dialog/helper/handleOpenDialog';
+	import ExerciseForm from '../form/exercise-form/ExerciseForm.svelte';
 	let { createdWorkout, children }: { createdWorkout: null | ExerciseJoined; children?: any } =
 		$props();
 </script>
@@ -13,7 +11,6 @@
 	type="button"
 	onclick={(e) =>
 		handleOpenDialog(e, {
-			// DialogContentComponent: WorkoutForm,
 			DialogContentComponent: ExerciseForm as any,
 			dialogContentProps: {
 				onSubmitCallback: (data: any) => {
