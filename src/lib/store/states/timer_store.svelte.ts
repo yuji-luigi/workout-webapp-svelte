4d07b346b2d4f784c40ff7c2940edcb1025116ab
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-01-15 09:36:02 +0100

    fix(pending): setting timer class to  control the timer behavior

--- src/lib/store/states/timer_store.svelte.ts
+++ src/lib/store/states/timer_store.svelte.ts
@@ -1,16 +1,17 @@
 import type { Interval, TimerKey } from '$types/db/interval';
 import type { RoutineJoined } from '../../../types/db/routine';
-
-class TimerController {
+export class TimerController {
 	currentTimer: Interval | null = null;
 	currentTimerKeys: TimerKey[] | null = null;
 	currentTimerIndex: number = 0;
 }
 
 // interval factory creates interval out of RoutineJoined, Interval[], or WorkoutSetJoined[] etc
-class TimerFactory {
+export class TimerFactory {
 	static fromRoutineJoined(routine: RoutineJoined) {
 		const interval = routine.workout_sets.map((set) => set.interval).flat();
 		return interval;
 	}
 }
+
+interface Timer {}
