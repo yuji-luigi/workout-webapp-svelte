2dd8ec60502abd3d2d4ad8b9d3db575964486818
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-01-25 15:45:45 +0100

    fix(new component): blackboard bg div

---
+++ src/lib/components/card/BlackBoardCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div class="blackboard">
+	{@render children()}
+</div>
+
+<style>
+	.blackboard {
+		background-image: url('/images/blackboard.jpg');
+		width: var(--width,);
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+</style>
