1e1cad175620c337138eeb1b5edf74e1d32f73de
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-02-15 11:32:01 +0100

    feat: show reps and update reps done

--- src/lib/components/routine-video/bottom-section-routine-video/RepsCount.svelte
+++ src/lib/components/routine-video/bottom-section-routine-video/RepsCount.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { isExerciseLog } from '../../../../types/db/routine_log';
+	import { getRoutineLogStore } from '../../../store/states/routine_log_store.svelte';
 	import { getCurrentRoutineStore } from '../../../store/states/routine_store.svelte';
 	import { getIntervalTimer } from '../../../store/timers/interval_timer.svelte';
 	import Button from '../../button/Button.svelte';
@@ -7,13 +9,19 @@
 	import MinusSvg from '../../icons/svgs/MinusSvg.svelte';
 	const currentRoutine = getCurrentRoutineStore();
 	const intervalTimer = getIntervalTimer();
+	const logStore = getRoutineLogStore();
 </script>
 
 <div>
-	<button onclick={() => console.log(currentRoutine)}>
+	<button onclick={() => logStore?.subCurrentExerciseReps()}>
 		<MinusSvg />
 	</button>
-	<button class="">
+	<p>
+		{logStore?.currentLog?.repetition_done.count}/{isExerciseLog(intervalTimer.currentFlow)
+			? intervalTimer.currentFlow.exercise.repetition.count
+			: 0}
+	</p>
+	<button onclick={() => logStore?.addCurrentExerciseReps()}>
 		<AddSvg />
 	</button>
 </div>
