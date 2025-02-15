a5a4c5c890fe9588fafb1c1aeabd200c9131a083
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-02-15 11:52:03 +0100

    fix(RoutineForm): modal not closing fixed

--- src/lib/components/form/form-by-collection/routine-form/RoutineForm.svelte
+++ src/lib/components/form/form-by-collection/routine-form/RoutineForm.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { setContext } from 'svelte';
 	import { routineFormTableJson } from '../../../../data/template-json/dataTable/routine-form-table-json';
-	import { Routine } from '../../../../db/dexie-db/Routine';
+	import { RoutineDexie } from '../../../../db/dexie-db/orm/RoutineDexie';
 	import { sleep } from '../../../../helpers/sleep';
 	import { setFormId } from '../../../../store/form-store.svelte';
-	import { closeDialog } from '../../../../store/global-dialog-store.svelte';
 	import { routinesStore } from '../../../../store/states/routine_store.svelte';
 	import JsonForm from '../../JsonForm.svelte';
+	import { closeStackDialogNew } from '../../../../store/dialog-stack/dialogStackStoreNew';
 
 	setFormId('routine-form');
 	setContext('form_table_json', routineFormTableJson);
@@ -15,10 +15,10 @@
 		payload: Record<string, any>
 	) {
 		try {
-			const newRoutine = await Routine.add(payload as any);
+			const newRoutine = await RoutineDexie.add(payload as any);
 			routinesStore.list = [...routinesStore.list, newRoutine];
 			await sleep(500);
-			closeDialog();
+			closeStackDialogNew();
 		} catch (error) {
 			console.error(error);
 		}
